0e8fba0055188f53c474c1024031a56db021b686
Robinnair <robinnair925@gmail.com>
2026-06-21 01:09:01 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    int []dp;
+    public int jump(int[] nums) {
+        int left=0;
+        int right=0;
+        int farthest=0;
+        int jumps=0;
+        while(right<nums.length-1){
+            for(int i=left;i<=right;i++){
+                farthest=Math.max(farthest,i+nums[i]);
+            }
+            left=right+1;
+            right=farthest;
+            jumps+=1;
+        }
+        return jumps;
+    }
+}
